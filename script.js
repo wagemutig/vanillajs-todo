92b1e06203d3ba8f30cc4773080a373c5edaa2e1
Colin Nico Schmid <mail@mala23.me>
2018-05-07 14:18:28 +0200

    create html elements on click

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.log('hello')
 
 var todoText = document.getElementById('todo-text')
-var todoCheckbox = document.getElementById('todo-checkbox')
 var todoAdd = document.getElementById('todo-add')
 
 function createTodo() {
   var todoTextValue = todoText.value
   console.log(todoTextValue)
+  var list = document.getElementById('todo-list')
+  var itemSection = document.createElement('section')
+  var itemLi = document.createElement('li')
+  var itemCheckbox = document.createElement('input')
+  itemCheckbox.setAttribute('type', 'checkbox');
+  list.appendChild(itemSection)
+  itemSection.appendChild(itemCheckbox)
+  itemSection.appendChild(itemLi)
+  itemLi.appendChild(document.createTextNode(todoTextValue))
+  todoText.value=''
 }
 
 todoText.addEventListener('keyup', function(event){
@@ -15,14 +24,3 @@ todoText.addEventListener('keyup', function(event){
     createTodo()
   }
 })
-
-/*
-todoText.addEventListener('click', function(){
-  var text = todoText.value
-  if (text == "") {
-    return
-  }
-  var todoItem = document.createElement('p')
-  todoItem.className = 'todo-item'
-}, false)
-*/
