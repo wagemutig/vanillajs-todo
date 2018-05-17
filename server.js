2777e8b1a3cfc834a3abf2a0a854530ddd5fca77
Colin Nico Schmid <mail@mala23.me>
2018-05-17 22:07:09 +0200

    make express work

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.use(require('./server/routes/routes'))
+app.use(express.static(__dirname + '/public'));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.listen(3000, () => console.log('app listening on port 300'))
+app.get('/', (req, res) => res.sendFile('index.html', { root: './public/html' }))
+
+app.listen(3000, () => console.log('app listening on port 3000'))
