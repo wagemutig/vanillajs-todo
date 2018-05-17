0b17bae3ec19573c55260f92ea1a71f6b68608df
Colin Nico Schmid <mail@mala23.me>
2018-05-17 22:35:20 +0200

    set up mongo

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var mongo = require('mongodb')
+
+var url = 'mongodb+srv://vanillajs-todo-jq2sk.mongodb.net:27017/test'
 
 app.use(express.static(__dirname + '/public'));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
